5a696e16cc23da9d5aaa77384fb8e6777019631c
Николай Калугин <dyson78@mail.ru>
2021-08-08 20:14:23 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,20 @@
 ﻿document.addEventListener("click", function (e) {
   if (e.target.className == "page-header__toggle") {
-    alert("click");
-    //ваши действия
+
+    alert("123");
+
+    // document.getElementsByClassName("page-header__toggle-open").classList.toggle("page__block--hide");
+    // document.getElementsByClassName("page-header__toggle-close").classList.toggle("page__block--hide");
+
+    // document.getElementsByClassName("main-nav").classList.toggle("page__block--hide");
+
+
+
+    // document.getElementsByClassName("main-nav").classList.add("page__block--show")
+
+
   }
+
 });
+
+
