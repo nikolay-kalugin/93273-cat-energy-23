e8e002a63e4d72022da902837943518acdd1a9d9
Николай Калугин <dyson78@mail.ru>
2021-08-08 21:03:56 +0300

    Настроено отображение мобильного меню

--- source/js/app.js
+++ source/js/app.js
@@ -1,18 +1,10 @@
 ﻿document.addEventListener("click", function (e) {
-  if (e.target.className == "page-header__toggle") {
 
-    alert("123");
-
-    // document.getElementsByClassName("page-header__toggle-open").classList.toggle("page__block--hide");
-    // document.getElementsByClassName("page-header__toggle-close").classList.toggle("page__block--hide");
-
-    // document.getElementsByClassName("main-nav").classList.toggle("page__block--hide");
-
-
-
-    // document.getElementsByClassName("main-nav").classList.add("page__block--hide")
-
-
+  if (e.target.className == "page-header__toggle-open" || e.target.className == "page-header__toggle-close") {
+    document.querySelector(".page-header__toggle-open").classList.toggle("page__block--hide");
+    document.querySelector(".page-header__toggle-close").classList.toggle("page__block--hide");
+    document.querySelector(".main-nav").classList.toggle("page__block--hide");
   }
 
+  // alert("123");
 });
