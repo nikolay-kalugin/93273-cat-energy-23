2f7904e007f85e5ebb01d9f0eca7f294dcffdd97
Николай Калугин <dyson78@mail.ru>
2021-08-18 15:06:30 +0300

    Добавлены эффекты hover & active

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,5 @@
-﻿document.addEventListener("click", function (e) {
+﻿// Анимация для значка бургер-меню
+document.addEventListener("click", function (e) {
 
   if (e.target.className == "page-header__toggle-open" || e.target.className == "page-header__toggle-close") {
     document.querySelector(".page-header__toggle-open").classList.toggle("page__block--hide");
@@ -6,5 +7,18 @@
     document.querySelector(".main-nav").classList.toggle("page__block--hide");
   }
 
-  // alert("123");
 });
+
+// Подчеркивание активного пункта меню
+const points = document.querySelectorAll(".main-nav__list li a");
+
+for (let point of points) {
+
+  if (point.href == document.location.href) {
+    point.style.borderBottom = "2px solid #68B738";
+    point.style.marginTop = "2px";
+    // point.style.paddingBottom = "3px";
+  }
+
+}
+
