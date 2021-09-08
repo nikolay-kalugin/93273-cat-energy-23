ad015589fd0585f7e231bf291c401b53a67853d9
Николай Калугин <dyson78@mail.ru>
2021-09-09 01:45:02 +0300

    Настраиваю автоматизацию сборки

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,19 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+const htmlmin = require("gulp-htmlmin");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const squoosh = require("gulp-libsquoosh");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+// const del = require("del");
+
 const sync = require("browser-sync").create();
 
-// Styles
+/********     Минификация CSS    *********/
 
 const styles = () => {
   return gulp.src("source/less/style.less")
@@ -14,21 +24,73 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    // .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+
+/*********      Минификация HTML      *********/
+
+const min_html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"))
+}
+
+exports.min_html = min_html;
+
+
+/*********       Минификация JS        *********/
+
+const min_js = () => {
+  return gulp.src("source/js/app.js")
+    .pipe(terser())
+    // .pipe(rename("app.min.js"))
+    .pipe(gulp.dest("build/js"))
+}
+
+exports.min_js = min_js;
+
+
+/*********     Оптимизация Images     *********/
+
+const opt_img = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}", { base: "source" })
+    .pipe(squoosh())
+    .pipe(gulp.dest("build"))
+}
+
+exports.opt_img = opt_img;
+
+/*********  Копирование fonts и favicons  *********/
+
+const copy_fonts_and_favicons = () => {
+  return gulp.src(["source/fonts/*.{woff,woff2}", "source/*.svg"], { base: "source" })
+    .pipe(gulp.dest("build"))
+}
+
+exports.copy_fonts_and_favicons = copy_fonts_and_favicons;
+
+
+
+
+
+
+
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build' // source
     },
     cors: true,
     notify: false,
@@ -47,5 +109,12 @@ const watcher = () => {
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  styles,
+  min_html,
+  min_js,
+  opt_img,
+  copy_fonts_and_favicons,
+
+  server,
+  watcher
 );
